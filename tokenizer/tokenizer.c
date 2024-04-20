ad673ecb7d53b0e2bedfd75b98807bcb04246e77
fra <frankvos61@gmail.com>
2024-04-20 17:34:51 +0100

    Im bad at tokenizing

--- tokenizer/tokenizer.c
+++ tokenizer/tokenizer.c
@@ -2,17 +2,32 @@
 #include <stdlib.h>
 
 
-void create_pair_list(int *id, int length, int *count){
-	
+void create_pair_list(int *id, int length, int (*count)[3]){
 	for(int i=0;i<length-1;i++){
-		for(int j=0;j<length-1;j++){
-			if(id[i] == id[j]){
-				printf("%d\n", id[i]);
-			}
-		}
+		count[i][0] = id[i];
+		count[i][1] = id[i+1];
+		count[i][2] = 0;
 	}
 }
 
+void find_common_pairs(int length, int (*count)[3]){
+	for(int i=0;i<length-1;i++) {
+        if(count[i][2] == -1) {
+            continue; 
+        }
+
+        for(int j=i+1;j<length-1;j++) {
+            if(count[j][0] == count[i][0] && count[j][1] == count[i][1]) {
+				++count[i][2];
+				count[j][2] = -1; // Mark as counted
+            }
+        }
+    }
+
+}
+
+void merge_pairs(int length, int (*count)[3]){
+}
 
 
 // Trainer
@@ -32,10 +47,14 @@ char ascii_to_char(int ascii_value) {
 int main(){
 	int id[] = {1, 2, 3, 1, 2, 3, 3, 1023, 1202, 12, 1201, 555, 555};
     int length = sizeof(id) / sizeof(id[0]);
-  
-	int count[length * length];
-
-    create_pair_list(id, length, count);
+	int count[length-1][3];
     
-    return 0;
+	create_pair_list(id, length, count);
+    find_common_pairs(length, count);
+   
+	printf("Pairs and their counts:\n");
+    for (int i = 0; i < length - 1; i++) {
+        printf("(%d, %d): %d\n", count[i][0], count[i][1], count[i][2]);
+    }
+	return 0;
 }
